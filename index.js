400268a0d390c9b93487c7c72b4330eea8773f0e
AvinashKotavenuka <158825834+AvinashKotavenuka@users.noreply.github.com>
2024-12-04 19:50:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.getElementById("myH1").textContent = "HAHAHAHA";
+// document.getElementById("myP").textContent = "everyone";
+
+window.alert("This is an alert");
+window.alert("This account is going hacked");
+window.alert("50% hacking done");
+
+window.alert("Your contacts are been shared");
+window.alert("You are been morphed");
